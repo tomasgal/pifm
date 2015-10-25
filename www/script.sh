404bff1126de6ec9175f978385a71215008a07ef
Tomas Gal <tomas.gal@upcmail.sk>
2015-10-25 21:00:41 +0100

    CSS Style

--- www/script.sh
+++ www/script.sh
@@ -5,4 +5,5 @@ sed -i '/MUSIC_ROOT\|delphigl/d' /var/www/pifm.log.htm
 sed -i 's/\<sudo -u www-data\>//g' /var/www/pifm.log.htm
 sed -i "8s/.*/<body onload='pageScroll()' style='color:white; background-color:black'>/" /var/www/pifm.log.htm
 sed -i "5i <script>function pageScroll() {window.scrollBy(0,50);scrolldelay = setTimeout('pageScroll()',20);}</script>" /var/www/pifm.log.htm
-
+sed -i "11i <p style='font-weight:normal;color:#E6E6E6;background-color:#000000;letter-spacing:0.3pt;word-spacing:1.2pt;font-size:10px;text-align:left;font-family:tahoma, verdana, arial, sans-serif;line-height:1;margin:0px;padding:0px;'>" /var/www/pifm.log.htm
+tail -n 4 pifm.log.htm | head -n 1 | sed 's/^ sox //' > now.txt
